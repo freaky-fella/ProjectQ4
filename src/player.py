802ee54ad754022c5eb81f89c99f57e7c43b1a1f
bean-man <9710594@graniteschools.org>
2026-04-14 10:30:26 -0700

    Update player.py

--- src/player.py
+++ src/player.py
@@ -1,1 +1,23 @@
+import pygame
+from settings import GROUND_Y, JUMP_STRENGTH, GRAVITY, CYAN
 
+class Player:
+    def __init__(self):
+        self.rect = pygame.Rect(100, GROUND_Y - 32, 32, 32)
+        self.vel_y = 0
+
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
+        if (keys[pygame.K_SPACE] or keys[pygame.K_UP]) and self.rect.bottom >= GROUND_Y:
+            self.vel_y = JUMP_STRENGTH
+
+    def apply_gravity(self):
+        self.vel_y += GRAVITY
+        self.rect.y += self.vel_y
+        
+        if self.rect.bottom > GROUND_Y:
+            self.rect.bottom = GROUND_Y
+            self.vel_y = 0
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, CYAN, self.rect)
